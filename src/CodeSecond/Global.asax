1627fd41d58a5f38dfaa50857c90e2d099c7a88c
Eric Anderson <eric@vector241.com>
2013-04-25 05:33:06 -0500

    Add basic MVC project

---
+++ src/CodeSecond/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeSecond.MvcApplication" Language="C#" %>
--- src/CodeSecond/Properties/AssemblyInfo.cs
+++ src/CodeSecond/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fbc588a2-1929-4de5-95cb-0c9461277eb2")]
+[assembly: Guid("27a7b1af-22c6-45bc-bd24-7bcafc10932f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
